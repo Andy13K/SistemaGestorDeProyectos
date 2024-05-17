8fdedc72bc3b800203a9222dee921720ec44bf3c
Andy13K <aaquinoe1@miumg.edu.gt>
2024-05-17 10:44:18 -0600

    Creando Proyecto con sus tablas

---
+++ database/migrations/2024_05_11_194727_create_auditorias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuditoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auditorias', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auditorias');
+    }
+}
