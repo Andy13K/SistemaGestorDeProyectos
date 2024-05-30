24e0a906a7bb287492179b308097b3156b5ed288
GABRI3L1009PCION <tonypicon98@gmail.com>
2024-05-30 01:21:33 -0600

    Realice el funcionamiendo de la barra progreso y la informacion que se mostrara en el show

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -16,7 +16,7 @@
             <strong>Categoría:</strong> {{ $proyecto->categoria->nombre ?? 'No asignado' }}
         </div>
         <div>
-            <strong>Líder:</strong> {{ $proyecto->lider->name ?? 'No asignado' }} <!-- Cambiado a 'name' -->
+            <strong>Líder:</strong> {{ $proyecto->lider->name ?? 'No asignado' }}
         </div>
         <div>
             <strong>Cliente:</strong> {{ $proyecto->cliente->nombre ?? 'No asignado' }}
@@ -32,6 +32,12 @@
         </div>
         <div>
             <strong>Fecha Límite:</strong> {{ $proyecto->fecha_limite }}
+        </div>
+        <div>
+            <h3>Progreso</h3>
+            <p>Total de Tareas: {{ $totalTareas }}</p>
+            <p>Tareas Completadas: {{ $tareasCompletadas }}</p>
+            <p>Porcentaje Completado: {{ round($proyecto->porcentajeCompletado, 2) }}%</p>
         </div>
         <a href="{{ route('proyectos.index') }}" class="btn btn-primary">Volver</a>
     </div>
